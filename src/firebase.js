e8399ac269efcb45324a102e1b2f0782278793d5
Rahul Ranjan <rahul.smile@gmail.com>
2020-05-24 20:04:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,7 @@ import "firebase/firestore";
 import "firebase/messaging";
 
 const config = {
-    apiKey: "AIzaSyA4k2W2tQ5o6qRConhckT628KtTAgYUUUE",
-    authDomain: "localhostproject-e2bae.firebaseapp.com",
-    databaseURL: "https://localhostproject-e2bae.firebaseio.com",
-    projectId: "localhostproject-e2bae",
-    storageBucket: "localhostproject-e2bae.appspot.com",
-    messagingSenderId: "465971440810",
-    appId: "1:465971440810:web:33ef55b58fd01dc6cc34d3"
+
 };
 
 const initFirebase = () => {
