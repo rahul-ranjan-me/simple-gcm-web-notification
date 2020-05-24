2e92594e10fdb25cf185d72c34acedba20924263
Rahul Ranjan <rahul.smile@gmail.com>
2020-05-24 20:05:21 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.14.5/firebase-messaging.js')
 
-const config = {
-    apiKey: "AIzaSyA4k2W2tQ5o6qRConhckT628KtTAgYUUUE",
-    authDomain: "localhostproject-e2bae.firebaseapp.com",
-    databaseURL: "https://localhostproject-e2bae.firebaseio.com",
-    projectId: "localhostproject-e2bae",
-    storageBucket: "localhostproject-e2bae.appspot.com",
-    messagingSenderId: "465971440810",
-    appId: "1:465971440810:web:33ef55b58fd01dc6cc34d3"
-};
+const config = {};
 
 if (!firebase.apps.length) {
     firebase.initializeApp(config);
